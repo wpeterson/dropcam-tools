e8c2046b025368649e97f6c1a5124a4b40976cfe
Winfield Peterson <winfield.peterson@gmail.com>
2013-12-31 15:52:04 -0500

    Adds bin/capture_screenshot tool

---
+++ lib/screenshot.rb
@@ -0,0 +1,23 @@
+require 'time'
+
+class Screenshot
+  def initialize(options = {})
+    @camera_uuid = options[:camera_uuid]
+    @filename = options[:filename]
+    @directory = options[:directory]
+  end
+
+  def api_url
+    "https://nexusapi.dropcam.com/get_image?width=800&uuid=#{ @camera_uuid }"
+  end
+
+  def local_file_path
+    return File.absolute_path( File.join(@directory, @filename) ) if @directory
+    @filename
+  end
+
+  def save!
+
+    system("curl -L --silent -o '#{ local_file_path }' '#{ api_url }'")
+  end
+end
